7cec89861656843b6135e7a07ee0ad3197dfad09
Gustavo Henrique Alves <he.gustavoalves@gmail.com>
2022-09-04 15:07:40 -0300

    I finished the testimonial section, as information from that section is still false, this needs to be changed later

--- js/main.js
+++ js/main.js
@@ -90,7 +90,7 @@ modalCloses.forEach((modalClose) => {
 });
 
 /* PORTFOLIO SWIPER */
-let swiper = new Swiper('.portfolio_container', {
+let swiperPortfolio = new Swiper('.portfolio_container', {
     cssMode: true,
     loop: true,
     navigation: {
@@ -100,5 +100,22 @@ let swiper = new Swiper('.portfolio_container', {
     pagination: {
         el: '.swiper-pagination',
         clickable: true,
+    },
+});
+
+/* TESTIMONIAL */
+let swiperTestimonial = new Swiper('.testimonial_container', {
+    loop: true,
+    grabCursor: true,
+    spaceBetween: 48,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
+    },
+    breakpoints: {
+        568: {
+            slidesPerView: 2,
+        },
     },
 });
